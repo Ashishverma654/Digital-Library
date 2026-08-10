803c3a79f7365bff24d3d99308c1134ac38ead1e
Ashish Verma <akumarverma654@gmail.com>
2026-08-10 14:56:50 +0530

    3rd commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,10 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL ? [process.env.CLIENT_URL, 'http://localhost:5173', 'http://localhost:5174'] : '*',
+  credentials: true,
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
